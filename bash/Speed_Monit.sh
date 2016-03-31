a8bce714c0a3525b7d0d196643e58152ce40b9be
= <robert@labinsightsllc.com>
2016-03-31 15:21:40 -0700

    New additions and generalizations to files.

--- bash/Speed_Monit.sh
+++ bash/Speed_Monit.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-timestamp = $(date +%H:%M)
-datestamp = $(date +%F)
-year      = $(date +%Y)
-month     = $(date +%m)
-day       = $(date +%d)
+# Gets data from speedtest.net and adds data to a csv file. Best used in a
+# cron job so that you can continuously monitor connection speeds.
+# Requires speedtest-cli
 
-mkdir -p /home/robert/Speedtest/$year/$month/
-touch "/home/robert/Speedtest/$year/$month/$datestamp.csv"
+timestamp=$(date +%H:%M)
+datestamp=$(date +%F)
+year=$(date +%Y)
+month=$(date +%m)
+day=$(date +%d)
 
-filesize=$(stat -c '%s' /home/robert/Speedtest/$year/$month/$datestamp.csv)
-if [ $filesize == "0" ] ; then echo "Time,Download,Upload,,Graphic" > /home/robert/Speedtest/$year/$month/$datestamp.csv ; fi
-speed_test_output=$(speedtest-cli --share --simple  --server 948)
+mkdir -p $HOME/Speedtest/$year/$month/
+touch "$HOME/Speedtest/$year/$month/$datestamp.csv"
+
+filesize=$(stat -c '%s' $HOME/Speedtest/$year/$month/$datestamp.csv)
+if [ $filesize == "0" ] ; then echo "Time,Download,Upload,,Graphic" > /home/pi/Speedtest/$year/$month/$datestamp.csv ; fi
+speed_test_output=$(/usr/bin/speedtest-cli --share --simple --server 948)
 download=$(echo "$speed_test_output" | grep ^Download | awk '{print $2}')
-upload=$(echo "$speed_test_output"   | grep ^Upload   | awk '{print $2}')
-graphic=$(echo "$speed_test_output"  | grep ^Share    | awk '{print $3}')
-
+upload=$(echo "$speed_test_output" | grep ^Upload | awk '{print $2}')
+graphic=$(echo "$speed_test_output" | grep ^Share | awk '{print $3}')
 if [ "$download" == "" ] ; then download="0" ; fi
-if [ "$upload" == ""   ] ; then upload="0" ; fi
-echo "$timestamp,$download,$upload,,$graphic" >> "/home/robert/Speedtest/$year/$month/$datestamp.csv"
-
+if [ "$upload" == "" ] ; then upload="0" ; fi
+echo "$timestamp,$download,$upload,,$graphic" >> "$HOME/Speedtest/$year/$month/$datestamp.csv"
