d587cbdb0e845a32139f42a6d403eba8bedc1ac2
ryoung29 <robert.e.young@gmail.com>
2016-12-29 11:00:36 -0800

    New script to change html clipboard to markdown

---
+++ bash/html_clip_to_markdown.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# This is good for turning html to markdown from the clipboard.
+# It's my replacement to getmd since the server is down. 
+# getmd was my replacement to the markdown chrome extension since
+# I changed to Firefox
+
+xclip -o selection clipboard -t text/html | pandoc -r html -w markdown_github || echo "command failed."
